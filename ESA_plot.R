3719a6a1068ceb6c76be39e7cad2213ad4904642
Christina Espada <christinaespada@Christinas-MBP.fios-router.home>
2022-11-03 10:19:02 -0400

    first commit

---
+++ ESA_plot.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+
+
+
+library(readxl)
+oct31 <- read_excel("~/Desktop/oct31.xlsx", 
+                    sheet = "Sheet3")
+View(oct31)
+library(readxl)
+Tissues <- read_excel("~/Desktop/2020-2021 Non-Ixodes Extracted Rodent Ticks and Tissues.xlsx", 
+                                                                       sheet = "Tissue")
+View(Tissues)
+df <- data.frame(oct31)
+
+library(ggplot2)
+
+ggplot(df, aes(AdultGCT, RodentPrev, color = Site)) + geom_point() +
+  geom_smooth(method = lm, se = FALSE)
+
+ggplot(df, aes(AdultGCT, RodentPrev)) + geom_point() +
+  geom_smooth(method = lm, se = FALSE)
+
+
+
+model <<- Tissues %>%
+  glm(formula = Rp ~ Year * Mammal, 
+      family = "binomial")
+
+summary(model)
+exp(cbind(coef(model), confint(model)))
